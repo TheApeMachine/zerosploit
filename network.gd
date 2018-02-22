0d0eca6314e5e1001051c9c696cc0004cb9f0c90
Daniel Owen van Dommelen <daniel.van.dommelen@gmail.com>
2018-02-22 05:21:32 +0100

    Moving player and camera code around and make networking work

--- network.gd
+++ network.gd
@@ -19,19 +19,29 @@ func start_server():
 	var err = host.create_server(33339, 10)
 	
 	if err != OK:
-		print("Network address in use!")
+		print("NETWORK: Network address in use!")
 		return
 		
 	get_tree().set_network_peer(host)
-	print("Waiting for other players...")
+	initialize_game()
+	
+	print("NETWORK: Waiting for other players...")
+	
+func join_server():
+	host = NetworkedMultiplayerENet.new()
+	host.set_compression_mode(NetworkedMultiplayerENet.COMPRESS_ZLIB)
+	host.create_client('127.0.0.1', 33339)
+	get_tree().set_network_peer(host)
+	print("NETWORK: Connecting to server...")
 	
 func _player_connected(id):
-	pass
+	print("NETWORK: _player_connected(", id, ")")
 	
 func _player_disconnected(id):
 	player_info.erase(id)
 	
 func _connected_ok():
+	print("NETWORK: _connected_ok()")
 	rpc("register_player", get_tree().get_network_unique_id(), my_info)
 	
 func _server_disconnected():
@@ -41,6 +51,7 @@ func _connected_fail():
 	pass
 	
 remote func register_player(id, info):
+	print("NETWORK: register_player(", id, ", ", info, ")")
 	player_info[id] = info
 	
 	if get_tree().is_network_server():
@@ -49,4 +60,16 @@ remote func register_player(id, info):
 		for peer_id in player_info:
 			rpc_id(id, "register_player", peer_id, player_info[peer_id])
 			
+remote func initialize_game():
+	var self_peer_id = get_tree().get_network_unique_id()
+	var my_player    = preload("res://player.tscn").instance()
+	
+	my_player.set_name(str(self_peer_id))
+	my_player.set_network_master(self_peer_id)
+	get_parent().add_child(my_player)
+	
+	for p in player_info:
+		var player = preload("res://player.tscn").instance()
+		player.set_name(str(p))
+		get_parent().add_child(player)
 	
